d109ea0e94a2db247374da64084c28b3b53a3590
Aurelien Benel <aurelien.benel@utt.fr>
2010-05-25 15:29:08 +0200

    List of texts (with links) sorted by corpus and title.

---
+++ lists/corpus.js
@@ -0,0 +1,20 @@
+function(head, req) {
+  var doc;
+  var corpus;
+  start({"headers":{"Content-Type" : "text/html"}});
+  send('<html><body><ul>');
+  while (doc = getRow()) {
+    if (doc.key[0]!=corpus) {
+      corpus = doc.key[0];
+      send("</ul><h1>");
+      send(corpus);
+      send("</h1><ul>");
+    }
+    send('<li><a href="../../_show/text/');
+    send(doc.id);
+    send('">');
+    send(doc.key[1]);
+    send('</a></li>');
+  }  
+  send('</ul></body></html>');
+}

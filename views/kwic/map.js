ed68a328e030388f01e0ffbd08f0752e199ff383
Aurelien Benel <aurelien.benel@utt.fr>
2010-06-03 15:26:33 +0200

    REFACTOR: moved kwic extraction to an inner function

--- views/kwic/map.js
+++ views/kwic/map.js
@@ -2,32 +2,38 @@ function(doc) {
   const CUTTER = /[\s\.;:\-,\!\?\)\(\]\[\{\}\'\`\’\"\″\“\”\«\»\\\/]/gi;
   const OFFSET = 35;
   const FRAME = 80
-  for each (p in doc.posts){
+
+  function extract(aString, begin, end) {
+    var preblank = "";
+    if (begin < 0) {
+      for (var i=0; i>begin; i--) {
+        preblank += '_';
+      }
+      begin = 0;
+    }
+    var postblank = "";
+    if (end > aString.length) {
+      const SUPPL = end - aString.length; 
+      for (var i=0; i<SUPPL; i++) {
+        postblank += '_';
+      }
+      end = aString.length;
+    }
+    return preblank + aString.substring(begin, end) + postblank;
+  }
+
+  for each (p in doc.posts) {
     var position = 0;
     var word = "";
-    for each (character in p.text){
+    for each (character in p.text) {
       position++;
       if (!character.match(CUTTER)) {
         word += character;
       } else if (word.length>0) {
-        var begin = position - (word.length + OFFSET);
-	var end = begin + FRAME;
-        var preblank = "";
-        if (begin < 0) {
-          for (var i=0; i>begin; i--) {
-            preblank += '_';
-          }
-          begin = 0;
-        }
-        var postblank = "";
-        if (end > p.text.length) {
-          const suppl = end - p.text.length; 
-          for (var i=0;i<suppl;i++) {
-            postblank += '_';
-          }
-	  end = p.text.length;
-        }
-        emit (word, preblank + p.text.substring(begin, end) + postblank);
+        const BEGIN = position - (word.length + OFFSET);
+        emit (word, {
+          text: extract(p.text, BEGIN, BEGIN + FRAME)
+        });
         word = "";
       }
     }

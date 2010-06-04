948a11ffa1c4cf14abc96302f5969c315a0496a2
Aurelien Benel <aurelien.benel@utt.fr>
2010-06-04 09:27:47 +0200

    FIX: KWIC display

--- lists/kwic.js
+++ lists/kwic.js
@@ -4,17 +4,24 @@ function(head, req) {
   start({"headers":{"Content-Type" : "text/html;charset=utf-8"}});
   send('<html><body><ul>');
   while (doc = getRow()) {
-    if (doc.key[0]!=pole) {
-      pole = doc.key[0];
-      send("</ul><h1>");
-      send(pole);
-      send("</h1><ul>");
+    if (doc.value.word) {
+      if (doc.value.word!=pole) {
+	pole = doc.value.word;
+	send("</ul><h1>");
+	send(pole);
+	send("</h1><ul>");
+      }
+      send('<li><a href="../../_show/text/');
+      send(doc.id);
+      send('#');
+      send(doc.value.begin);
+      send('-');
+      send(doc.value.end);
+      send('"><pre>');
+      send(doc.value.before);
+      send(doc.key[1]);
+      send('</pre></a></li>');
     }
-    send('<li><a href="../../_show/text/');
-    send(doc.id);
-    send('"><pre>');
-    send(doc.key[1]);
-    send('</pre></a></li>');
   }  
   send('</ul></body></html>');
 }

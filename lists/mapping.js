18c55f158097338eb96f5fd7f5ed95caca98707e
Aurelien Benel <aurelien.benel@utt.fr>
2010-06-04 19:07:01 +0200

    FIX: When a pattern subsumes another (e.g. "foo" and "foobar") in the same corpus

--- lists/mapping.js
+++ lists/mapping.js
@@ -6,32 +6,43 @@ function(head, req) {
 
   send('[\n');
   var corpus;
-  var pattern = "a wannabe pattern";
+  var patterns = ["a wannabe pattern"];
   while (r = getRow()) {
     if (r.value.broader) {
-      corpus = r.key[0]; 
-      pattern = r.key[1];
-    } else if (r.key[1].startsWith(pattern) && r.key[0]==corpus) {
-      var key = [r.key[0], r.id, r.value.begin, r.value.end];
-      var json = { key: key, value: {
-        topic: pattern, 
-        text: r.value.before + r.key[1]
-      }};
-      send(JSON.stringify(json));
-      send(',\n');
-      var resource  = '../../_show/text/';
-      resource += r.id;
-      resource += '#';
-      resource += r.value.begin;
-      resource += '-';
-      resource += r.value.end;
-      json = {key: key, value: {resource: resource}};
-      send(JSON.stringify(json));
-      send(',\n');
-      if (r.value.author) {
-	json = {key: key, value: {speaker: r.value.author}};
-	send(JSON.stringify(json));
-	send(',\n');
+      if (r.key[1].startsWith(patterns[0])  && r.key[0]==corpus) {
+        patterns.push(r.key[1]);
+      } else {
+	corpus = r.key[0]; 
+	patterns = [r.key[1]];
+      }
+    } else {
+      var p = 0;
+      while (
+        p<patterns.length 
+        && r.key[1].startsWith(patterns[p]) 
+        && r.key[0]==corpus
+      ){
+        var key = [r.key[0], r.id, r.value.begin, r.value.end];
+        var json = { key: key, value: {
+          topic: patterns[p++], 
+          text: r.value.before + r.key[1]
+        }};
+        send(JSON.stringify(json));
+        send(',\n');
+        var resource  = '../../_show/text/';
+        resource += r.id;
+        resource += '#';
+        resource += r.value.begin;
+        resource += '-';
+        resource += r.value.end;
+        json = {key: key, value: {resource: resource}};
+        send(JSON.stringify(json));
+        send(',\n');
+        if (r.value.author) {
+          json = {key: key, value: {speaker: r.value.author}};
+          send(JSON.stringify(json));
+          send(',\n');
+        }
       }
     }
   }  

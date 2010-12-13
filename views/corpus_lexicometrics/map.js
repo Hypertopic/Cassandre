c958e39b0aa00ee6cf006a2967aae72201696ceb
Aurelien Benel <aurelien.benel@utt.fr>
2010-12-13 21:53:48 +0100

    FIX: "Specific words" feature needs the number of documents where a word can be found.

--- views/corpus_lexicometrics/map.js
+++ views/corpus_lexicometrics/map.js
@@ -1,9 +1,17 @@
 function(doc) {
   const ALPHA = /[a-zàâçéêèëïîôöüùû0-9]+/gi;
+  var counts = {};
   for each (var p in doc.speeches) {
-    var speechWords = p.text.match(ALPHA);
-    if (speechWords) for each (var w in speechWords) {
-      emit([w.toLowerCase()], 1);
+    var words = p.text.match(ALPHA);
+    if (words) for each (var t in words) {
+      t = t.toLowerCase();
+      if (!counts[t]) {
+        counts[t] = 0;
+      }
+      counts[t]++;
     }
+  }
+  for (var word in counts) {
+    emit([word], counts[word]);
   }
 }

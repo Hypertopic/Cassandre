421b74e85d148d15ceb346449af04f3b3570f81b
Christophe Lejeune <christophe.lejeune@ulg.ac.be>
2010-12-23 21:58:23 +0100

    FIX: Special case for the "constructor" exception

--- views/corpus_lexicometrics/map.js
+++ views/corpus_lexicometrics/map.js
@@ -2,6 +2,7 @@ function(doc) {
   const ALPHA = /[a-zàâçéêèëïîôöüùû0-9]+/gi;
   var counts = {};
   counts["watch"] = 0;
+  counts["constructor"] = 0;
   for each (var p in doc.speeches) {
     var words = p.text.match(ALPHA);
     if (words) for each (var t in words) {

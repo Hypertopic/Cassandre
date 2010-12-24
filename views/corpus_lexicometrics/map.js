ff0fed04b5831301f0ad2d273437091b64834391
Christophe Lejeune <christophe.lejeune@ulg.ac.be>
2010-12-24 13:04:41 +0100

    FIX: Neater management of "watch" & "constructor" exceptions

--- views/corpus_lexicometrics/map.js
+++ views/corpus_lexicometrics/map.js
@@ -1,13 +1,11 @@
 function(doc) {
   const ALPHA = /[a-zàâçéêèëïîôöüùû0-9]+/gi;
   var counts = {};
-  counts["watch"] = 0;
-  counts["constructor"] = 0;
   for each (var p in doc.speeches) {
     var words = p.text.match(ALPHA);
     if (words) for each (var t in words) {
       t = t.toLowerCase();
-      if (!counts[t]) {
+      if (!counts.hasOwnProperty(t)) {
         counts[t] = 0;
       }
       counts[t]++;

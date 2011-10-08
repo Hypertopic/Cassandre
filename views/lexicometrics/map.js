916ca482bdc7c518467d634fc021977a4f358852
Aurelien Benel <aurelien.benel@utt.fr>
2011-10-08 16:34:34 +0200

    REFACTORING: Count words and phrases in a single loop in order to speed up view generation.

--- views/lexicometrics/map.js
+++ views/lexicometrics/map.js
@@ -1,12 +1,64 @@
 function(o) {
-  if (o.draft!='ongoing') {
-    const ALPHA = /[a-zàâçéêèëïîôöüùû0-9]+/gi;
-    for each (var p in o.speeches) {
-      var speechWords = p.text.match(ALPHA);
-      if (speechWords) for each (var w in speechWords) {
-        emit([o._id, w.toLowerCase()]);
-        emit([o.corpus, w.toLowerCase()]);
+  const WORD_PART = /[a-zàâçéêèëïîôöüùû0-9]/i;
+  const CRUNCHER = /[a-zàâçéêèëïîôöüùû0-9]+|[^a-zàâçéêèëïîôöüùû0-9]+/gi;
+
+  function emitWord(corpus, doc, word) {
+    emit([corpus, word]);
+    emit([doc, word]);
+  }
+
+  function emitPhrase(corpus, doc, prior, previous, current) {
+    emit([corpus, prior, previous, current]);
+    emit([doc, prior]);
+  }
+
+  function emitEndOfSentence(corpus, doc, prior, previous, current) {
+    if (current) {
+      emitWord(corpus, doc, current);
+      if (previous) {
+        emitWord(corpus, doc, previous);
+        if (prior) {
+          emitPhrase(corpus, doc, prior, previous, current);
+        }
       }
+    }
+  } 
+
+  function emitPartOfSentence(corpus, doc, prior, previous, current) {
+    if (current && previous && prior) {
+      emitPhrase(corpus, doc, prior, previous, current);
+    }
+  }
+
+  function isWord(string) {
+    return string[0].match(WORD_PART);
+  }
+
+  if (o.draft!='ongoing') {
+    var current = null;
+    var previous = null;
+    var prior = null;
+    for each (var s in o.speeches) {
+      for each (var m in s.text.match(CRUNCHER)) {
+        if (isWord(m)) {
+          prior = previous;
+          previous = current;
+          current = m.toLowerCase();
+        } else {
+          if (m.length>1) {
+            emitEndOfSentence(o.corpus, o._id, prior, previous, current);
+            prior = null;
+            previous = null;
+            current = null;
+          } else {
+            emitPartOfSentence(o.corpus, o._id, prior, previous, current);
+          }
+        }
+      }
+      emitEndOfSentence(o.corpus, o._id, prior, previous, current);
+      prior = null;
+      previous = null;
+      current = null;
     }
   }
 }

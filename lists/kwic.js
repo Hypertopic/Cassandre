f0009cefddde05f747cee62708f654bef30e82bf
Christophe Lejeune <christophe.lejeune@ulg.ac.be>
2012-06-18 08:07:30 +0200

    FIX: Kwic includes document IDs

--- lists/kwic.js
+++ lists/kwic.js
@@ -8,6 +8,7 @@ function(head, req) {
   while (row = getRow()) {
     data.occurrences.push({
       corpus: row.key[0],
+      id: row.id,
       before: row.value.before,
       match: row.key[1]
     });

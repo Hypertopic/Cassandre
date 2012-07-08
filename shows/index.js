c2003d47ea3ba6a1c8d1160869014f6fccfeed8f
Aurelien Benel <aurelien.benel@utt.fr>
2012-07-08 15:40:00 +0200

    API: update_seq as a database metadata.

--- shows/index.js
+++ shows/index.js
@@ -2,8 +2,11 @@ function(o, req) {
   // !json templates.index
   if('Accept' in req.headers && req.headers['Accept'].indexOf('json')>0) {
     return {
-      body: JSON.stringify({'service': 'Cassandre', 'revision': '2.12.03.10'}),
-      headers: { 
+      body: JSON.stringify({
+        service: 'Cassandre', 
+        revision: '2.12.07.08', 
+        update_seq: req.info.update_seq
+      }), headers: { 
         "Content-Type": "application/json",
       }
     }

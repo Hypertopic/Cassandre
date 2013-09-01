27ec6aa388e6e3dc555cf400086bc6e88a5f69fa
Aurelien Benel <aurelien.benel@utt.fr>
2013-09-01 21:48:43 +0200

    RELEASE: 2.13.09.01

--- shows/index.js
+++ shows/index.js
@@ -5,7 +5,7 @@ function(o, req) {
     return {
       body: JSON.stringify({
         service: 'Cassandre', 
-        revision: '2.12.07.08', 
+        revision: '2.13.09.01',
         update_seq: req.info.update_seq
       }), headers: { 
         "Content-Type": "application/json",

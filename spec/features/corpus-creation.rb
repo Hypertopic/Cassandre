35e73cf3dc01b4035c131188a363306a28c78f19
Christophe Lejeune <christophe.lejeune@ulg.ac.be>
2014-05-21 22:03:20 +0200

    TEST: Create a corpus (#32)

---
+++ spec/features/corpus-creation.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+
+feature 'Create a corpus (including one text)' do
+
+  $a_title = a_string()
+  $a_corpus = a_string()
+
+  background do
+    visit '/'
+    click_on 'Analyse qualitative de textes'
+    click_on '+'
+    fill_in 'name', :with => $a_title
+    fill_in 'corpus', :with => $a_corpus
+    click_on 'Enregistrer les attributs'
+  end
+  
+  scenario 'checking if corpus exists' do
+    click_on 'Corpus'
+    page.should have_content $a_corpus
+  end
+
+end

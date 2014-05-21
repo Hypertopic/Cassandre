5513628399bc5a6296e9f8e6899dcb91fe2dd486
Christophe Lejeune <christophe.lejeune@ulg.ac.be>
2014-05-21 21:52:16 +0200

    TEST: Create a text (#31)

---
+++ spec/features/text-creation.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+feature 'Create a text' do
+
+  $a_title = a_string()
+  $a_corpus = a_string()
+
+  background do
+    visit '/'
+    click_on 'Analyse qualitative de textes'
+    click_on '+'
+  end
+  
+  scenario 'filling (title and corpus) fields' do
+    fill_in 'name', :with => $a_title
+    fill_in 'corpus', :with => $a_corpus
+    click_on 'Enregistrer les attributs'
+    page.should have_content 'Tour de parole'
+  end
+
+end

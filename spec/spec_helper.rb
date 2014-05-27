e5f9a8e580d49363babc8a2e09a3b235e4e76001
Aurelien Benel <aurelien.benel@utt.fr>
2014-05-27 16:01:39 +0200

    REFACTORING: Removed unused test helper functions.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -20,24 +20,3 @@ def prefer_language(language)
   page.driver.header 'Accept-Language', language
 end
 
-def field(id)
-  find_by_id(id).value
-end
-
-# for select boxes handled by Chosen
-def fill_in_and_select(value, options)
-  options[:visible] = false
-  select value, options
-end
-
-def wait_until
-  require "timeout"
-  Timeout.timeout(Capybara.default_wait_time) do
-    sleep(0.1) until value = yield
-    value
-  end
-end
-
-def downloaded_file_name
-  wait_until {page.response_headers['Content-Disposition']}.partition('=').last
-end

c2156904b75b38c5eb8acb908b1fbdee16aeaee9
Aurelien Benel <aurelien.benel@utt.fr>
2014-08-26 10:06:14 +0200

    FIX: Reduce repeated phrases payload.

---
+++ lists/phrase.js
@@ -0,0 +1,14 @@
+function(head, req) {
+  send('{"rows":[');
+  var row = getRow();
+  var first = true;
+  while (row) {
+    if (row.value>1) { // phrase is repeated
+      if (!first) send(',\n');
+      send(toJSON(row));
+      first = false;
+    }
+    row = getRow();
+  }
+  send(']}');
+}

188cffc40e0fed3778c38210dca64056c9b33b63
Christophe Lejeune <christophe.lejeune@ulg.ac.be>
2016-01-18 13:20:41 +0100

    FEATURE: List memos belonging to one selected diary (#55).

---
+++ lists/diary.js
@@ -0,0 +1,22 @@
+function(head, req) {
+  // !json templates.diary
+  // !code lib/mustache.js
+  // !code l10n/l10n.js
+  start({"headers":{"Content-Type":"text/html;charset=utf-8"}});
+  var data = {
+    i18n: localized(),
+    by: req.query.by,
+    diary: req.query.diary,
+    memos: []
+  };
+  while (row = getRow()) {
+    data.memos.push({
+      diary: row.key,
+      id: row.value.id,
+      name: row.value.name,
+      date: row.value.date,
+      type: row.value.type
+    });
+  }
+  return Mustache.to_html(templates.diary, data);
+}

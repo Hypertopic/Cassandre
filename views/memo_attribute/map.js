bd37f34c038970b615572e0e98786990efafd316
Christophe Lejeune <christophe.lejeune@ulg.ac.be>
2016-02-06 12:42:07 +0100

    REFACTORING: Diagrams are listed together with memos.
    Any memo or diagram is listed, even if name, date or content is missing.

--- views/memo_attribute/map.js
+++ views/memo_attribute/map.js
@@ -1,23 +1,21 @@
 function(o) {
-  if (o.diary) {
-    var diary = o.diary;
-    emit([diary], {
-      id: o._id,
-      name: o.name,
-      type: o.type,
-      date: o.date,
-      preview: o.body.substr(0, 200)
-    });
-  } else if (o.name) {
-    var diary = o.corpus;
-    var date = o.date || '';
-    var preview = o.speeches[0].text.substr(0, 200) || '';
-    emit([diary], {
-      id: o._id,
-      name: o.name,
-      type: 'field',
-      date: date,
-      preview: preview
-    });
+  var diary = o.diary || o.corpus;
+  var type = o.type || 'field';
+  var date = o.date || ' ';
+  var name = o.name || ' ';
+  if (o.body) {var preview = o.body.substr(0, 200);
+  } else {
+  if (o.speeches) {
+    var preview = o.speeches[0].text.substr(0, 200) || ' ';
+    } else {
+    var preview = ' ';
+    }
   }
+  emit([diary], {
+    id: o._id,
+    name: name,
+    type: type,
+    date: date,
+    preview: preview
+  });
 }

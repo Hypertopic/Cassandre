974930999f412ba214e620257f03d649354034b5
Christophe Lejeune <christophe.lejeune@ulg.ac.be>
2017-03-21 15:43:33 +0100

    FIX: More robust diagram rendering.
    The diagram should look the same on different version of the browser.

--- lists/diagram.js
+++ lists/diagram.js
@@ -1,39 +1,136 @@
-function(o, req){
+function(head, req) {
   // !json templates.diagram
   // !code lib/mustache.js
   // !code l10n/l10n.js
-  var type = o.type;
-  var diary = o.diary || o.corpus;
-  var data = {
-    i18n: localized(),
-    _id: o._id,
-    _rev: o._rev,
-    authorized: !o.readers || o.readers.indexOf(req.userCtx.name)>-1 || o.contributors && o.contributors.indexOf(req.userCtx.name)>-1 || req.userCtx.roles.indexOf("_admin")>-1,
-    diary: diary,
-    link: o.link,
-    logged: req.userCtx.name,
-    type: type,
-    name: o.name,
-    date: o.date,
-    groundings: [],
-  }
-  for each (var g in o.groundings) {
-    var grounding = {
-      id: g.value
-    };
-    data.groundings.push(g);
-  }
-  if (o.contributors) {
-    data.contributors = [];
-    for each (var c in o.contributors) {
-      data.contributors.push(c);
-    }
-  }
-  if (o.readers) {
-    data.readers = [];
-    for each (var r in o.readers) {
-      data.readers.push(r);
-    }
+
+  start({"headers":{"Content-Type":"text/html;charset=utf-8"}});
+  var fullnames = [];
+  while (row = getRow()) {
+    switch (row.key[1]) {
+      case ('C'):
+        var id = row.value._id;
+        var fullname = id;
+        data.contributors.push(id);
+        if (row.doc) fullname = row.doc.fullname;
+        if (row.doc && !fullnames[id]) fullnames[id] = fullname;
+        data.contributors_fullnames.push(fullname);
+      break;
+      case ('D'):
+        if (row.doc) data.diary_name = row.doc.diary_name;
+      break;
+      case ('R'):
+        var id = row.value._id;
+        var fullname = id;
+        data.readers.push(id);
+        if (row.doc) fullname = row.doc.fullname;
+        if (row.doc && !fullnames[id]) fullnames[id] = fullname;
+        data.readers_fullnames.push(fullname);
+      break;
+      case ('M'):
+        var user = row.value._id;
+        if (row.doc && !fullnames[user]) fullnames[user] = row.doc.fullname;
+        if (row.doc) user = row.doc.fullname;
+        data.comments.push({
+          user: user,
+          date: row.value.date,
+          text: row.value.text,
+          id: row.value.id,
+          rev: row.value.rev
+        });
+      break;
+      case ('H'):
+        var user = row.value._id;
+        if (row.doc && !fullnames[user]) fullnames[user] = row.doc.fullname;
+        if (row.doc) user = row.doc.fullname;
+        data.date = user+', '+row.value.date;
+      break;
+      case ('G'):
+        if (row.doc)  {
+          if (row.doc.body) {
+            var preview = row.doc.body.substr(0, 200).replace(/\s/g, ' ');
+          } else {
+            if (row.doc.speeches) {
+              var preview = row.doc.speeches[0].text.substr(0, 200) || ' ';
+            } else {
+              var preview = ' ';
+            }
+          }
+          var ground_type = row.doc.type || 'field';
+          switch (ground_type) {
+            case ('diagram'):
+            var ground_path = '';
+            break;
+            case ('graph'):
+            var ground_path = '../../graph/'+diary+'/';
+            break;
+            default:
+            var ground_path = '../../memo/'+diary+'/';
+          }
+          data.groundings.push({
+            id: row.value._id,
+            href: ground_path + row.value._id,
+            type: ground_type,
+            preview: preview,
+            name: row.doc.name
+          });
+        }
+      break;
+      case ('L'):
+        var type = row.doc.type || 'field';
+        var id = row.doc._id;
+        var href = row.doc._id;
+        var name = row.doc.name || '...';
+        switch (type) {
+          case ('diagram'):
+            break;
+          case ('graph'):
+            var href = '../../graph/'+diary+'/'+href;
+            break;
+          default:
+            var href = '../../memo/'+diary+'/'+href;
+        }
+        data.leaves.push({
+          href: href,
+          id: id,
+          name: name,
+          type: type
+        });
+      break;
+      default:
+      var username = req.userCtx.name;
+      var type = row.doc.type || 'field';
+      var diary = row.doc.diary || row.doc.corpus;
+      var data = {
+        i18n: localized(),
+        _id: row.doc._id,
+        _rev: row.doc._rev,
+        authorized: !row.doc.readers || row.doc.readers.indexOf(username)>-1 || row.doc.contributors && row.doc.contributors.indexOf(username)>-1 || req.userCtx.roles.indexOf("_admin")>-1,
+        body: [],
+        contributors: [],
+        contributors_fullnames: [],
+        comments: [],
+        date: row.doc.date,
+        diary: diary,
+        groundings: [],
+        peer: req.peer,
+        leaves: [],
+        logged: username,
+        logged_fullname: username,
+        name: row.doc.name.replace(/\s/g, ' '),
+        readers: [],
+        readers_fullnames: [],
+        roles: req.userCtx.roles,
+        type: type
+      }
+      if (row.doc.link) {
+        data.link = row.doc.link;
+        if (row.doc.negative) {
+          data.negative = row.doc.negative;
+        }
+      }
+      break;
+      }
+    if (fullnames[username]) data.logged_fullname = fullnames[username];
   }
   return Mustache.to_html(templates.diagram, data);
 }

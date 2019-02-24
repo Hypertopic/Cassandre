Hypertopic/Cassandre

commit 5c64f073cc0a1e9da405e32d594e107e2159183a

FIX: Preventing paragraphs starting with a tabulation to be rendered as code.
... by removing the tabulation.

lists/memo.js
@@ -138,7 +138,7 @@ function(head, req) {
       data.locale = data.locale[0].substring(0,2);
       if (row.doc.body) {
         var content = {
-          text: row.doc.body.replace(/\n {4,}/g, "\n").replace(/\n\n/g, "\n \n")
+          text: row.doc.body.replace(/\n[ \t]+/g, "\n").replace(/\n\n/g, "\n \n")
         };
         data.body.push(content);
       } else {

commit 0a10b6e706c23c3cc3f74a7b1e7a6d55825f0314

FIX: Disabling MarkDown convention for code (continued).
Removes tabular indentations likely to shape the paragraph as code (due to ShowdownJS library) while preserving sublists (broken since commit 5c64f073cc0a1e9da405e32d594e107e2159183a).

This commit continues commit 09aa0ec4ca3e6fb4a3910cc39c6f22eeb31fd12d.

lists/memo.js
@@ -138,7 +138,7 @@ function(head, req) {
       data.locale = data.locale[0].substring(0,2);
       if (row.doc.body) {
         var content = {
-          text: row.doc.body.replace(/\n[ \t]+/g, "\n").replace(/\n\n/g, "\n \n")
+          text: row.doc.body.replace(/\n\t/g, "\n ").replace(/\n {4,}/g, "\n").replace(/\n\n/g, "\n \n")
         };
         data.body.push(content);
       } else {
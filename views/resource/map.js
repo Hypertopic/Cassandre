9ad172f8064da520daef13df86e56a864ac6b226
Christophe Lejeune <christophe.lejeune@ulg.ac.be>
2019-04-23 20:39:28 +0200

    FIX: Compatibility with Hypertopic item (fixes #84).

--- views/resource/map.js
+++ views/resource/map.js
@@ -1,5 +1,6 @@
 function(o) {
-  if (o.corpus) {
-    emit(o._id, {corpus:o.corpus, item:o._id});
+  if (o.diary || o.corpus) {
+    var corpus = o.diary || o.corpus;
+    emit(o._id, {corpus:corpus, item:o._id});
   }
 }

8aad2e6484c515add6ea3fc9354852518a60c0d7
Aurelien Benel <aurelien.benel@utt.fr>
2019-06-14 11:48:09 +0200

    FIX: Get the item's attributes corresponding to a memo (closes #88).

--- views/item/map.js
+++ views/item/map.js
@@ -1,5 +1,23 @@
 function(o) {
+
+  function isReserved(key) {
+    switch (key) {
+      case "_id":
+      case "_rev":
+      case "corpus":
+      case "speeches":
+      case "highlights": return true;
+    }
+    return false;
+  }
+
   if (o.name && o.corpus) {
-    emit([o.corpus, o._id], {name: o.name});
+    for (var key in o) {
+      if (!isReserved(key)) {
+        var entry = {};
+        entry[key] = o[key];
+        emit([o.corpus, o._id], entry);
+      }
+    }
   }
 }

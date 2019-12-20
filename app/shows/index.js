a0ba35a7f6cf08c02d843bae628e1a6d76dff9f0
Christophe Lejeune <christophe.lejeune@ulg.ac.be>
2019-12-20 16:07:51 +0100

    RELEASE: 3.19.12.20
    Improving process, layout, productivity, collaboration, performance and compatibility.
    
    Process:
    * Easier installation (thanks to Docker).
    
    Layout:
    * Introducing static graphs;
    * Responsive KWICs.
    
    Productivity and collaboration:
    * Saving sorting preference from one session to another;
    * Preventing simultaneous modifications of a memo;
    * Notifying current modifications.
    
    Performance:
    * Incremental list of memos composing the diary;
    * Incremental list of modification in the history of the diary and in the user page.
    
    Compatibility:
    * Better interaction with LaSuli.

--- app/shows/index.js
+++ app/shows/index.js
@@ -7,7 +7,7 @@ function(o, req) {
     return {
       body: JSON.stringify({
         service: 'Cassandre',
-        revision: '3.19.06.23',
+        revision: '3.19.12.20',
         update_seq: req.info.update_seq
       })
     }

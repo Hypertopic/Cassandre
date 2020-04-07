09f31243c54dd3ea695b723b3570d27e5a749b5f
Christophe Lejeune <christophe.lejeune@ulg.ac.be>
2020-04-07 15:56:23 +0200

    FIX: Repeated phrases are computed only for interviews (#104)

--- app/views/phrase/map.js
+++ app/views/phrase/map.js
@@ -1,22 +1,20 @@
 const CRUNCHER = /[a-zàâçéêèëïîôöüùûæœ0-9]+|[^a-zàâçéêèëïîôöüùûæœ0-9]+/gi;
 
 function(o) {
-  if (!o.editing) {
-    for each (var s in o.speeches) {
-      var chunks = s.text.match(CRUNCHER);
-      if (chunks) for (var i=0; i<chunks.length-4; i+=2) {
-        if (
-          (chunks[i].length>3 || chunks[i+2].length>3 || chunks[i+4].length>3)
-          && chunks[i+1].length==1
-          && chunks[i+3].length==1
-        ) {
-          emit([
-            o.corpus,
-            chunks[i].toLowerCase(),
-            chunks[i+2].toLowerCase(),
-            chunks[i+4].toLowerCase()
-          ]);
-        }
+  if (o.type == 'interview' && !o.editing) {
+    var chunks = o.body.match(CRUNCHER);
+    if (chunks) for (var i=0; i<chunks.length-4; i+=2) {
+      if (
+        (chunks[i].length>3 || chunks[i+2].length>3 || chunks[i+4].length>3)
+        && chunks[i+1].length==1
+        && chunks[i+3].length==1
+      ) {
+        emit([
+          o.diary,
+          chunks[i].toLowerCase(),
+          chunks[i+2].toLowerCase(),
+          chunks[i+4].toLowerCase()
+        ]);
       }
     }
   }

32aec2958076bf83a40cdfd633d2f7f5d172819a
Christophe Lejeune <christophe.lejeune@ulg.ac.be>
2020-05-25 09:18:56 +0200

    REFACTORING: Activity follows access rights
    Instead of showing all activities independently from user rights, the activity page henceforth shows creations, modifications and comments of memos that the user is allowed to access. This feature prevents to show all activities (and, incidentally all memo names) of a diary to a user who is allowed to read only one memo of this diary.
    
    This refactoring is done at list level; as #98 suggests for task-list, a deeper (view level) refactoring could be more efficient.

--- app/lists/activity.js
+++ app/lists/activity.js
@@ -62,49 +62,53 @@ function(head, req) {
         }
       break;
       case ('Z'):
-        var object = {
-            user: row.value._id,
-            date: row.key[2]
-        };
-        if (row.doc && row.doc.fullname) {
-          if(!fullnames[row.value._id]) fullnames[row.value._id] = row.doc.fullname;
-          object.user = row.doc.fullname;
-        }
-        if (row.value.modified_name) object.modified_name = row.value.modified_name;
-        if (row.value.modified_id)   object.modified_id = row.value.modified_id;
-        if (row.value.diary_label)   object.diary_label = 1;
-        if (row.value.created) {
-          object.created = 'created';
-          if (nCreated[object.user] == null) {
-            nCreated[object.user] = '1';
-          } else {
-            nCreated[object.user]++;
+        var contributors = [].concat(row.doc.readers, row.doc.contributors).sort();
+        var users = contributors.filter(function(item, pos, ary) {return !pos || item != ary[pos - 1];} );
+        if (row.doc.readers == undefined || row.doc.readers.length == 0 || users.indexOf(req.userCtx.name) > -1) {
+          var object = {
+              user: row.value._id,
+              date: row.key[2]
+          };
+          if (row.doc && row.doc.fullname) {
+            if(!fullnames[row.value._id]) fullnames[row.value._id] = row.doc.fullname;
+            object.user = row.doc.fullname;
           }
-        }
-        if (row.value.modified) {
-          object.modified = 'modified';
-          if (nModified[object.user] == null) {
-            nModified[object.user] = '1';
-          } else {
-            nModified[object.user]++;
+          if (row.value.modified_name) object.modified_name = row.value.modified_name;
+          if (row.value.modified_id)   object.modified_id = row.value.modified_id;
+          if (row.value.diary_label)   object.diary_label = 1;
+          if (row.value.created) {
+            object.created = 'created';
+            if (nCreated[object.user] == null) {
+              nCreated[object.user] = '1';
+            } else {
+              nCreated[object.user]++;
+            }
           }
-        }
-        if (row.value.comment) {
-          object.commented = 'commented';
-          object.modified_name = row.value.comment;
-          if (nCommented[object.user] == null) {
-            nCommented[object.user] = '1';
-          } else {
-            nCommented[object.user]++;
+          if (row.value.modified) {
+            object.modified = 'modified';
+            if (nModified[object.user] == null) {
+              nModified[object.user] = '1';
+            } else {
+              nModified[object.user]++;
+            }
           }
+          if (row.value.comment) {
+            object.commented = 'commented';
+            object.modified_name = row.value.comment;
+            if (nCommented[object.user] == null) {
+              nCommented[object.user] = '1';
+            } else {
+              nCommented[object.user]++;
+            }
+          }
+          object.modified_path = memos_path[object.modified_id];
+          data.activity.push(object);
         }
-        object.modified_path = memos_path[object.modified_id];
-        data.activity.push(object);
       break;
     }
   }
   provides("html", function() {
-    data.end = data.activity[data.activity.length-1].date;
+    if (data.activity.length > 0) data.end = data.activity[data.activity.length-1].date;
     if (fullnames[req.userCtx.name]) data.logged_fullname = fullnames[req.userCtx.name];
     for(var user in nCommented) {
       data.ncommented.push({

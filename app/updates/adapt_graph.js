82883aa3fe251c7f1b7f5a7d83bc0ead73c21b15
Christophe Lejeune <christophe.lejeune@uliege.be>
2020-12-07 09:24:55 +0100

    FEATURE: Integrating all articulations at once
    When adding groundings to a graph, the user may integrate all available articulations in one click.

--- app/updates/adapt_graph.js
+++ app/updates/adapt_graph.js
@@ -5,6 +5,25 @@ function (doc, req) {
       diagram = body.diagram;
   switch(action) {
     case ('add_grounding'):
+      integrate(diagram);
+    break;
+    case ('integrate_everything'):
+      for (var i = 0; i < diagram.length; i++) {
+        integrate(diagram[i]);
+      }
+    break;
+    case ('remove_grounding'):
+      var i = doc.groundings.indexOf(diagram.id);
+      if (i > -1) doc.groundings.splice(i, 1);
+    break;
+    case ('clean_nodes'):
+      doc.nodes = doc.nodes.filter(function(a) { if (connected_nodes.indexOf(a.id) > -1) return a;});
+      keep_trace = false;
+    break;
+  }
+  return [doc, 'Graph updated'];
+
+  function integrate(diagram) {
       var nodeids = doc.nodes.map(function(a) {return a.id;});
       if (doc.groundings.indexOf(diagram.id) == -1) doc.groundings.push(diagram.id);
       switch (diagram.link) {
@@ -23,15 +42,5 @@ function (doc, req) {
           if(nodeids.indexOf(diagram.groundings[1]) == -1) doc.nodes.push({id: diagram.groundings[1], shape: 'diamond'});
           break;
       }
-    break;
-    case ('remove_grounding'):
-      var i = doc.groundings.indexOf(diagram.id);
-      if (i > -1) doc.groundings.splice(i, 1);
-    break;
-    case ('clean_nodes'):
-      doc.nodes = doc.nodes.filter(function(a) { if (connected_nodes.indexOf(a.id) > -1) return a;});
-      keep_trace = false;
-    break;
   }
-  return [doc, 'Graph updated']
 }

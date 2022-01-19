fcad207805bbddeddfa66380085009221aef690b
Christophe Lejeune <christophe.lejeune@uliege.be>
2022-01-19 09:08:56 +0100

    REFACTORING: Mutualising `add grounding` function

---
+++ app/_attachments/grounding.js
@@ -0,0 +1,70 @@
+$('#add_grounding').on('show.bs.modal', function (event) {
+  if (this_type == "graph") refresh = false;
+  candidates = [];
+  $('#select_grounding').children("option").each(function(){
+    var c = $(this).val();
+    if (this_type == "graph" && c != $("#select_grounding").children('option').first().text()) c = JSON.parse(c).id;
+    if($(this).val() != $("#select_grounding").children('option').first().text()) candidates.push(c);
+  })
+  var url = "../../memo_attribute/"+diary_id;
+  if (this_type == "graph") url = "../../memo_type/"+diary_id+"/diagram";
+  if (this_type == "table") url = "../../memo_type/"+diary_id+"/coding";
+  $.ajax({
+    url: url,
+    type: "GET",
+    dataType: "json",
+  }).done(function(ground_candidates) {
+    for (var i in ground_candidates.rows) {
+      var r = ground_candidates.rows[i],
+          id = r.value['id'],
+          type = '',
+          name = r.value['name'];
+      if (this_type == "graph") var obj = {'id': id, 'groundings': r.value['groundings'], 'link': r.value['link']};
+      if (this_type != "graph") type = r.value['type'];
+      if (name.length > 45) name = name.substr(0,45)+'...';
+      if(document.getElementById(id) == null && id != this_id && candidates.indexOf(id) < 0) {
+        if (["graph","table"].indexOf(this_type) == -1) $('#type_filter select option[value="' + type + '"]').removeAttr("hidden");
+        if (this_type == "graph") {
+          id = JSON.stringify(obj);
+          articulations.push(obj);
+        }
+        $("#select_grounding").append($('<option>', {
+          title: r.value['name'],
+          text: name,
+          class: type,
+          value: id
+        }));
+      }
+    }
+    $('#loading').remove();
+    $('#type_filter').removeAttr("hidden");
+    $('#select_grounding').removeAttr("hidden");
+    $('#all-articulations').parent().removeAttr("hidden");
+    $("#one-articulation").prop("checked", true).trigger("click");
+    switch (this_type) {
+      case ('field'):
+      case ('transcript'):
+      case ('interview'):
+        $('#type_filter select option[value="operational"]').prop("selected", true);
+        filter('operational');
+        break;
+      case ('coding'):
+        $('#type_filter select option[value="interview"]').prop("selected", true);
+        filter('interview');
+        break;
+      case ('theoretical'):
+        $('#type_filter select option[value="coding"]').prop("selected", true);
+        filter('coding');
+        break;
+      case ('operational'):
+        $('#type_filter select option[value="theoretical"]').prop("selected", true);
+        filter('theoretical');
+        break;
+      case ('storyline'):
+        $('#type_filter select option[value="diagram"]').prop("selected", true);
+        filter('diagram');
+        break;
+    }
+  });
+})
+

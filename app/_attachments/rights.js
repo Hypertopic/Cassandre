5d9d8d8083b1fcd13d3f2c1aa7e824783628f077
Christophe Lejeune <christophe.lejeune@uliege.be>
2022-02-02 10:25:37 +0100

    REFACTORING: Mutualising `rights` scripts

---
+++ app/_attachments/rights.js
@@ -0,0 +1,82 @@
+$('#add_contributor').on('keypress', function(key) {
+  if (key.which == 13) {
+    modify_rights('add_contributor', $('#add_contributor').val().trim().toLowerCase());
+  }
+});
+$('#add_contributor').autocomplete({
+  minLength: 3,
+  appendTo: '#modify_rights_dialog',
+  source: function(request, response) {
+    $.getJSON('../../userlist/' + request.term, function (data) {
+      response($.map(data.rows, function (value, key) {
+        if (contributors.split(',').concat(userids).indexOf(value.id) == -1) {
+          userids.push(value.id);
+          return {
+            label: value.value.fullname,
+            value: value.id
+          };
+        }
+      }));
+      userids = [];
+    });
+  },
+  select: function (event, ui) {
+    modify_rights('add_contributor', ui.item.value);
+  }
+});
+$('#add_reader').on('keypress', function(key) {
+  if (key.which == 13) {
+    modify_rights('add_reader', $('#add_reader').val().trim().toLowerCase());
+  }
+});
+$('#add_reader').autocomplete({
+  minLength: 3,
+  appendTo: '#modify_rights_dialog',
+  source: function(request, response) {
+    $.getJSON('../../userlist/' + request.term, function (data) {
+      response($.map(data.rows, function (value, key) {
+        if (readers.split(',').concat(userids).indexOf(value.id) == -1) {
+          userids.push(value.id);
+          return {
+            label: value.value.fullname,
+            value: value.id
+          };
+        }
+      }));
+      userids = [];
+    });
+  },
+  select: function (event, ui) {
+    modify_rights('add_reader', ui.item.value);
+  }
+});
+$('.remove_reader').on('click', function() {
+  modify_rights('remove_reader', $(this).val());
+});
+$('.remove_contributor').on('click', function() {
+  modify_rights('remove_contributor', $(this).val());
+});
+$('#unsubscribe').on('click', function() {
+  $.ajax({
+    url: '../../reader_unsubscribe/'+this_id,
+    type: 'PUT',
+    contentType: 'application/json'
+  });
+});
+function modify_rights(action, value) {
+  $.ajax({
+    url: '../../modify_rights/'+this_id,
+    type: 'PUT',
+    contentType: 'application/json',
+    data: JSON.stringify({
+     'action': action,
+     'value': value
+    }),
+    error: function(request) {
+      alert(
+        (JSON.parse(request.responseText).reason || request.responseText)
+        + '\\nCode ' + request.status
+      );
+    }
+  });
+}
--- app/lib/shared.js
+++ app/lib/shared.js
@@ -171,9 +171,12 @@ var shared = {
     <script src='{{>relpath}}script/moment.min.js'></script>\
     <script src='{{>relpath}}script/showdown.min.js'></script>\
     <script src='{{>relpath}}script/layout.js'></script>\
-    <script src='{{>relpath}}script/render.js'></script>\
     <script src='{{>relpath}}script/log.js'></script>\
+    {{^list}}\
+    <script src='{{>relpath}}script/render.js'></script>\
+    <script src='{{>relpath}}script/rights.js'></script>\
     <script src='{{>relpath}}script/comment.js'></script>\
+    {{/list}}\
     <link rel='stylesheet' href='{{>relpath}}style/jquery-ui.min.css' />",
   layoutscript:"\
   {{^list}}\
@@ -252,89 +255,6 @@ var shared = {
       });\
     }\
   };",
-  rightsscript: "\
-  $('#add_contributor').on('keypress', function(key) {\
-    if (key.which == 13) {\
-      modify_rights('add_contributor', $('#add_contributor').val().trim().toLowerCase());\
-    }\
-  });\
-  $('#add_contributor').autocomplete({\
-    minLength: 3,\
-    appendTo: '#modify_rights_dialog',\
-    source: function(request, response) {\
-      $.getJSON('../../userlist/' + request.term, function (data) {\
-        response($.map(data.rows, function (value, key) {\
-          if ('{{contributors}}'.split(',').concat(userids).indexOf(value.id) == -1) {\
-            userids.push(value.id);\
-            return {\
-              label: value.value.fullname,\
-              value: value.id\
-            };\
-          }\
-        }));\
-        userids = [];\
-      });\
-    },\
-    select: function (event, ui) {\
-      modify_rights('add_contributor', ui.item.value);\
-    }\
-  });\
-  $('#add_reader').on('keypress', function(key) {\
-    if (key.which == 13) {\
-      modify_rights('add_reader', $('#add_reader').val().trim().toLowerCase());\
-    }\
-  });\
-  $('#add_reader').autocomplete({\
-    minLength: 3,\
-    appendTo: '#modify_rights_dialog',\
-    source: function(request, response) {\
-      $.getJSON('../../userlist/' + request.term, function (data) {\
-        response($.map(data.rows, function (value, key) {\
-          if ('{{readers}}'.split(',').concat(userids).indexOf(value.id) == -1) {\
-            userids.push(value.id);\
-            return {\
-              label: value.value.fullname,\
-              value: value.id\
-            };\
-          }\
-        }));\
-        userids = [];\
-      });\
-    },\
-    select: function (event, ui) {\
-      modify_rights('add_reader', ui.item.value);\
-    }\
-  });\
-  $('.remove_reader').on('click', function() {\
-    modify_rights('remove_reader', $(this).val());\
-  });\
-  $('.remove_contributor').on('click', function() {\
-    modify_rights('remove_contributor', $(this).val());\
-  });\
-  $('#unsubscribe').on('click', function() {\
-    $.ajax({\
-      url: '../../reader_unsubscribe/{{_id}}',\
-      type: 'PUT',\
-      contentType: 'application/json'\
-    });\
-  });\
-  function modify_rights(action, value) {\
-    $.ajax({\
-      url: '../../modify_rights/{{_id}}',\
-      type: 'PUT',\
-      contentType: 'application/json',\
-      data: JSON.stringify({\
-       'action': action,\
-       'value': value\
-      }),\
-      error: function(request) {\
-        alert(\
-          (JSON.parse(request.responseText).reason || request.responseText)\
-          + '\\nCode ' + request.status\
-        );\
-      }\
-    });\
-  }",
   logscript: "\
   const everyone = '{{i18n.i_everyone}}',\
         enter_comment ='{{i18n.i_enter_comment}}',\

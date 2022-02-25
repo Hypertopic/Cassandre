7d3878cdba166d7dd908bfc594464845dcbc4b62
Christophe Lejeune <christophe.lejeune@uliege.be>
2022-02-25 17:04:04 +0100

    FIX: Recording or deleting search patterns

--- app/views/corpus_pattern/map.js
+++ app/views/corpus_pattern/map.js
@@ -1,11 +1,13 @@
 function(o) {
-  var corpus = o.corpus || o._id;
-  for (var [h, highlight] of Object.entries(o.highlights)) {
-    emit(corpus, {
-      highlight: h,
-      text: highlight.text,
-      viewpoint: highlight.viewpoint,
-      topic: highlight.topic
-    });
+  if (o.highlights) {
+    var diary = o.corpus || o.diary || o._id;
+    for (var [h, highlight] of Object.entries(o.highlights)) {
+      emit(diary, {
+        highlight: h,
+        text: highlight.text,
+        viewpoint: highlight.viewpoint,
+        topic: highlight.topic
+      });
+    }
   }
 }

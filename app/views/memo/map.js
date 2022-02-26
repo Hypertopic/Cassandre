d5a3a884ab0d0626f3f2d6807d0cf4ae177ed581
Christophe Lejeune <christophe.lejeune@uliege.be>
2022-02-26 17:09:17 +0100

    REFACTORING: Securing `for of` loops
    Checking that the array/object exists before entering iteration.

--- app/views/memo/map.js
+++ app/views/memo/map.js
@@ -4,7 +4,7 @@ function(o) {
     emit([o._id, 'D'], {'_id': o.diary});
   if (o.corpus)
     emit([o._id, 'D'], {'_id': o.corpus});
-  for (var g of o.groundings) {
+  if (o.groundings) for (var g of o.groundings) {
     if (typeof g === 'string' || g instanceof String) {
       emit([o._id, 'G'], {'_id': g});
       emit([g, 'L']);
@@ -17,9 +17,9 @@ function(o) {
     emit([o.commented, 'M', o.date], {'_id': o.user, 'date': o.date, 'text': o.text, 'checked': o.checked, 'id': o._id});
   if (o.history)
     emit([o._id, 'H'], {'_id': o.history[0].user, 'date': o.history[0].date});
-  for (var c of o.contributors)
+  if (o.contributors) for (var c of o.contributors)
     emit([o._id, 'C'], {'_id': c});
-  for (var r of o.readers)
+  if (o.readers) for (var r of o.readers)
     emit([o._id, 'R'], {'_id': r});
   if (o.nodes) for (var n of o.nodes)
     emit([o._id, 'N'], {'_id': n.id, 'shape': n.shape});

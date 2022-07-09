22fa2c4b0922f472f7e7f2b563d353e8e9df8c64
Christophe Lejeune <christophe.lejeune@uliege.be>
2022-07-09 20:31:35 +0200

    REFACTORING: Unsubscribing from a memo (#101)
    Unsubscribing from a memo does not affect other readers rights

--- app/updates/reader_unsubscribe.js
+++ app/updates/reader_unsubscribe.js
@@ -1,10 +1,20 @@
 function (doc, req) {
-  doc.readers.splice(doc.readers.indexOf(req.userCtx.name), 1);
-  if (doc.readers.length == 0) {
-    if ((typeof doc.history[0].user !== 'undefined') && (doc.history[0].user !== req.userCtx.name)) {
-      doc.readers.push(doc.history[0].user);
-    } else {
-      doc.readers.push(doc.contributors[0]);
+  if (doc.readers) {
+    switch (doc.readers.length) {
+      case 0:
+      break;
+      case 1:
+        if (doc.readers[0] == req.userCtx.name) {
+          if (doc.contributors[0] !== 'undefined') {
+            doc.readers = [doc.contributors[0]];
+          } else if (typeof doc.history[0].user !== 'undefined' && doc.history[0].user !== req.userCtx.name){
+            doc.readers = [doc.history[0].user];
+          }
+        }
+      break;
+      default:
+        doc.readers.splice(doc.readers.indexOf(req.userCtx.name), 1);
+      break;
     }
   }
   return [doc, 'User removed']

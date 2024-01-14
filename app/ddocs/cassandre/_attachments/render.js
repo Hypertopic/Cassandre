e402e809870af287be844b2f054c90a66678e3c2
Christophe Lejeune <christophe.lejeune@uliege.be>
2024-01-14 14:16:08 +0100

    REFACTORING: Each memo page mentions his/her creator (#127)
    A color is attributed to the creator tag.

--- app/ddocs/cassandre/_attachments/render.js
+++ app/ddocs/cassandre/_attachments/render.js
@@ -3,11 +3,23 @@ function render(){
   renderLeaves()
   momentRelative('')
   $('#memo_creator')
-    .append($('#creator>.username').text().split(" ").map((n) => n[0]).join('').toUpperCase())
+    .append($('#creator>.username').text().split(/[ -]+/).map((n) => n[0]).join('').toUpperCase())
     .attr('title', $('#creator').text())
     .removeClass('hidden')
+  coloringInitials($('#creator>.username').text())
   stickToHeader()
   let refresh = true;
+}
+function coloringInitials(fullname){
+  let initials = fullname.split(/[ -]+/).map((n) => n[0]),
+      rgb = [];
+  for (i of initials) rgb.push((i.charCodeAt() - 60) * 16)
+  if (initials.length === 2) rgb.splice(1, 0, fullname.length * 7)
+  $('#memo_creator').css('background-color', 'rgb('+rgb.join(',')+')')
+  if (rgb.reduce((a, b) => a + b, 0) > 600)
+    $('#memo_creator')
+      .addClass('text-dark')
+      .addClass('border-dark')
 }
 function renderLeaves(){
   if ($('#name').val().length > 0) {

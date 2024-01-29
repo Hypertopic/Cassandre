a8ade66a84fc064093b2446dd16a12cc1d714dde
Christophe Lejeune <christophe.lejeune@uliege.be>
2024-01-29 09:29:48 +0100

    FIX: Diacritics are excluded from creator tag

--- app/ddocs/cassandre/_attachments/layout.js
+++ app/ddocs/cassandre/_attachments/layout.js
@@ -474,7 +474,7 @@ function momentRelative(what) {
 function coloringCreatorTag(userid){
   let name = fullnames[userid]
   if (typeof (name) === 'undefined' || name.length < 1) name = userid
-  let initials = name.split(/[ -\.]+/).map((n) => n[0].normalize('NFD').replace(/\p{Diacritic}/gu, '')),
+  let initials = name.split(/[ -\.]+/).map((n) => n.substr(0,1).normalize('NFD').replace(/\p{Diacritic}/gu, '').toUpperCase()).filter((e) => e !== ''),
       rgb = [],
       contrast = 'light';
   for (i of initials) rgb.push((i.charCodeAt() - 60) * 16)

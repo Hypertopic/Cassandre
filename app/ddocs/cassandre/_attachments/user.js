3b9b07a07f56d2e17cd56e815d84da1b78b3fd5f
Christophe Lejeune <christophe.lejeune@uliege.be>
2024-02-11 15:21:10 +0100

    REFACTORING: Mutualising activity functions
    An external js file prevents to integrate functions in the computation of (user or diary) activity. This should lighten loading.

---
+++ app/ddocs/cassandre/_attachments/user.js
@@ -0,0 +1,66 @@
+$(window).scroll(function(){
+  if ($(window).scrollTop() == $(document).height() - $(window).height()){
+    showMore($("li").last().find("span").attr('id'));
+  }
+});
+$('#diaries').on('click', function() {
+  self.location = '../diary/';
+});
+$('#next').on('click', function() {
+  showMore($("li").last().find("span").attr('id'));
+});
+$('#all-activity').on('click', function() {
+  cal.update(all);
+});
+$('#only-created').on('click', function() {
+  cal.update(created);
+});
+$('#only-commented').on('click', function() {
+  cal.update(commented);
+});
+$('#only-modified').on('click', function() {
+  cal.update(modified);
+});
+function showMore(start) {
+  $.ajax({
+    url: '../user/'+user_id+'/'+start,
+    type: "GET",
+    dataType: "json"
+  }).done(function(data){
+    for (var e of data) {
+      $("#events").append('<li class="'+e.action+'">'
+        +'<span id="'+e.date+'" class="'+e.date+' moment"></span>&nbsp;– '
+        +'<a href="../'+e.type+'/'+e.id+'">'+e.name+'</a>'
+        +'</li>');
+    }
+  }).done(function(){
+    momentRelative('#events');
+    $('.commented>a').html(function(i, text) {
+      var md = converter.makeHtml(text.replace(/&gt;/g, '>').replace(/\[([^\]]*)\]\([^\)]*\)/, '$1').trim())
+      if (md.substring(0,3) == '<p>') md = md.replace(/<\/?p>/g, '');
+      return md;
+    });
+  }).then(function(){
+    if ($('#events li').length == 0) $('#events').text(nothing_to_show);
+  });
+}
+
+function drawChart(array) {
+  cal.init({
+    domain: "week",
+    subDomain: "day",
+    subDomainDateFormat: function(date) {
+      return hr_date(date);
+    },
+    subDomainTitleFormat: {
+      filled: "{count} {name}, {date}"
+    },
+    data: array,
+    range: 53,
+    start: start,
+    end: end,
+    itemName: ["contribution", "contributions"],
+    domainLabelFormat: ""
+  });
+}
+

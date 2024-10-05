3894316421d73031099e7cd66c8fdd0dbdbd23f9
Christophe Lejeune <christophe.lejeune@uliege.be>
2024-10-05 18:55:41 +0200

    FIX: Locally storing names of the diaries (#97)
    The local cache is cleaned when a diary access is removed.
    
    Thanks to Lola Dente !

--- app/ddocs/cassandre/shows/index.js
+++ app/ddocs/cassandre/shows/index.js
@@ -7,7 +7,7 @@ function(o, req) {
     return {
       body: JSON.stringify({
         service: 'Cassandre',
-        revision: '3.24.07.29',
+        revision: '3.24.10.05',
         update_seq: req.info.update_seq
       })
     }

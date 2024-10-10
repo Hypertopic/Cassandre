acfd3ba78779c11be4931ac85d47af6071101416
Christophe Lejeune <christophe.lejeune@uliege.be>
2024-10-10 15:32:08 +0200

    FIX: Locally storing names of the diaries (#97)
    As soon as a diary access is removed, the diary is removed from the 
    diaries page.

--- app/ddocs/cassandre/shows/index.js
+++ app/ddocs/cassandre/shows/index.js
@@ -7,7 +7,7 @@ function(o, req) {
     return {
       body: JSON.stringify({
         service: 'Cassandre',
-        revision: '3.24.10.05',
+        revision: '3.24.10.10',
         update_seq: req.info.update_seq
       })
     }

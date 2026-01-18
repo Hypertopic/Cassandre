e2fdb7f3aaaf6e65991e44a070f8d73be0b27ea0
Christophe Lejeune <christophe.lejeune@uliege.be>
2026-01-18 21:50:38 +0100

    STYLE: Responsive navbars.
    Minor improvements.

--- app/ddocs/cassandre/shows/index.js
+++ app/ddocs/cassandre/shows/index.js
@@ -7,7 +7,7 @@ function(o, req) {
     return {
       body: JSON.stringify({
         service: 'Cassandre',
-        revision: '3.26.01.17',
+        revision: '3.26.01.18',
         update_seq: req.info.update_seq
       })
     }

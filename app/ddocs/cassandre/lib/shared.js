4a04c33de8fd20ae46eba810d222f58cfb8eeedb
Christophe Lejeune <christophe.lejeune@uliege.be>
2026-01-28 21:00:36 +0100

    REFACTORING: Button tooltips for comments

--- app/ddocs/cassandre/lib/shared.js
+++ app/ddocs/cassandre/lib/shared.js
@@ -232,12 +232,12 @@ let shared = {
       <textarea id='comment_input' rows='1' type='text' class='form-control hidden' autocomplete='off'></textarea>\
     </div>",
   commentsbtn:"\
-    <button class='btn navbar-btn btn-sm' id='comment_create' data-toggle='tooltip' data-placement='bottom' title='{{i18n.i_comment}}'>\
+    <button class='btn navbar-btn btn-sm' id='comment_create' data-toggle='tooltip' data-placement='top' title='{{i18n.i_comment}}'>\
       <svg class='bi' width='24' height='24' fill='currentColor'>\
         <use xlink:href='../style/bootstrap-icons.svg#chat-right-text'/>\
       </svg>\
     </button>\
-    <button class='btn btn-primary hidden' type='button' id='commented' data-toggle='tooltip' data-placement='bottom' title='{{i18n.i_save}}'>\
+    <button class='btn btn-primary hidden' type='button' id='commented' data-toggle='tooltip' data-placement='top' title='{{i18n.i_save}}'>\
       <svg class='bi' width='24' height='24' fill='currentColor'>\
         <use xlink:href='../style/bootstrap-icons.svg#check-lg'/>\
       </svg>\
